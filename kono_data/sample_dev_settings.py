0367c96934aa29b916d3da244c88eae582be4224
hellno <holgerniessner@me.com>
2018-05-07 21:22:28 +0200

    - add sample_dev_settings - remove prod_settings

---
+++ kono_data/sample_dev_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
