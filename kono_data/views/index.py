aad20d9c7418c3e4116f8d119240d6c8c19b3b53
hellno <holgerniessner@me.com>
2018-05-19 12:29:22 +0200

    minor fixes and improvements
    tasks and keys in separate DB fields, not in json array
    fix index datasets display, now showing all datasets that a user has access to

--- kono_data/views/index.py
+++ kono_data/views/index.py
@@ -18,8 +18,7 @@ class IndexView(TemplateView):
             datasets = Dataset.objects.filter(is_public=True).order_by('-id')
         else:
             user = self.request.user
-            datasets = Dataset.objects.filter(Q(is_public=True) &
+            datasets = Dataset.objects.filter(Q(is_public=True) |
                                               (Q(user=user) | Q(admins__id=user.id) | Q(contributors__id=user.id)))
-        print(datasets)
         context['datasets'] = annotate_datasets_for_view(datasets, context['view'].request.user)[:10]
         return context
