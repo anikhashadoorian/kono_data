1fd5976b388b19514324c6f8ad257dcc8a32c691
hellno <holgerniessner@me.com>
2018-07-22 15:02:31 +0200

    fix test

--- kono_data/views/user.py
+++ kono_data/views/user.py
@@ -30,7 +30,7 @@ def signup(request, **kwargs):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
