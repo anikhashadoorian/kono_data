70545c7c1e0e06abbf2becb6926c76ca4c836c2b
Holger <holger@runningco.de>
2019-06-26 20:55:04 +0200

    add zappa_settings.json

--- zappa_deploy.sh
+++ zappa_deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 zappa update prod
-# zappa invoke prod "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'nimda')" --raw
 zappa manage prod migrate
 zappa manage prod "collectstatic --noinput"
-# terminal-notifier -title "KonoData Deploy" -message "Done 🚀"
 
+# terminal-notifier -title "KonoData Deploy" -message "Done 🚀"
+# zappa invoke prod "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'nimda')" --raw
 # python manage.py dumpdata --format=json data_model > data_model/fixtures/initial_data.json
 # zappa manage prod "loaddata data_model/fixtures/initial_data.json"
