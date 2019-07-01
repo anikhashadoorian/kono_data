c5d83cca7f7d33a03966bacde689970cbdfd5537
holger <holger@zeitgold.com>
2019-07-01 18:39:55 +0200

    fix travis.yml

--- build_ionic.sh
+++ build_ionic.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 cd kono-data-frontend
+#npm install
 ionic build --prod -- --base-href /static/ionic/
-mv www/* ../data_model/static/ionic
+rm -rf ../data_model/static/ionic/
+mkdir ../data_model/static/ionic/
+cp -rf www/* ../data_model/static/ionic/
